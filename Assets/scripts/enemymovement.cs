617e90bf3478a0df02ac3d97540e131ebf12cd34
KhaosOwO <kingofwinner2@gmail.com>
2025-01-01 21:30:23 +0800

    enemy movement

---
+++ Assets/scripts/enemymovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float changeDirectionInterval = 2f;
+
+    private Vector3 moveDirection;
+    private float timeSinceLastChange = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastChange += Time.deltaTime;
+
+        if (timeSinceLastChange >= changeDirectionInterval)
+        {
+            ChangeDirection();
+            timeSinceLastChange = 0f;
+        }
+
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        // Flip the enemy to face the movement direction
+        if (moveDirection.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void ChangeDirection()
+    {
+        float randomAngle = Random.Range(0f, 360f);
+        moveDirection = new Vector3(Mathf.Cos(randomAngle * Mathf.Deg2Rad), 0, Mathf.Sin(randomAngle * Mathf.Deg2Rad)).normalized;
+    }
+}
