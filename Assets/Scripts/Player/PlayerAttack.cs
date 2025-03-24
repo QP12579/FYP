a54036ff6f461fa1419b522a85b20ed908294e7f
QP12579 <164116570+QP12579@users.noreply.github.com>
2025-03-25 01:21:37 +0800

    weak weak normal Attack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     [Header("Attack")]
     public float attack = 5;
+    public float waitTime = 0.5f;
+    private bool canAtk = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        canAtk = true;
+    }
 
     public void OnTriggerStay(Collider c)
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && canAtk)
+        {
+            canAtk = false;
+            LeanTween.delayedCall(waitTime, DetectOnce);
+            animator.SetTrigger("normalATK");
+            print("NATK");
             if (c.gameObject.GetComponent<Enemy>())
                 c.gameObject.GetComponent<Enemy>().TakeDamage(attack);
-        
+        }
+    }
+
+    private void DetectOnce()
+    {
+        canAtk = true;
     }
 }
