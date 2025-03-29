09e4efa7bd2bc98a337d6949c5c84287d5524c62
cky314 <164114393+cky314@users.noreply.github.com>
2025-03-30 00:43:40 +0800

    Skill Tree Ability/Skill and Special Attack UI + import DOTween

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,185 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject GamingUI;
+
+    public GameObject SkillOrAbilityPanel;
+    public GameObject SpecialAttackPanel;
+
+    public Scrollbar SkillAbilityscrollbar;
+    public Scrollbar SpecialAttackscrollbar;
+
+    public RectTransform cyanSelectedBlock;
+    private bool isIncreasing = false;
+    private bool isDecreasing = false;
+    
+    public Button arrowButton; 
+    public Button skillOrAbilityButton;
+    public Button specialAttackButton;
+    public Button EseButton;
+
+    public GameObject SkillClickArea;
+    public GameObject AbilityClickArea;
+
+    public float scrollSpeed = 0.1f;
+
+    private void Start()
+    {
+        GamingUI.SetActive(false);
+        SkillAbilityscrollbar.value = 0;
+
+        arrowButton.onClick.AddListener(OnArrowButtonClick);
+        skillOrAbilityButton.onClick.AddListener(OnSkillOrAbilityButtonClick);
+        specialAttackButton.onClick.AddListener(OnSpecialAttackButtonClick);
+        EseButton.onClick.AddListener(OnEseButtonClick);
+
+        UpdateButtonActive();
+
+        SkillClickArea.SetActive(false);
+        AbilityClickArea.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) { GamingUI.SetActive(true); }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) { GamingUI.SetActive(false); }
+
+        // get mouse scroll value
+        float scrollDelta = Input.mouseScrollDelta.y;
+
+        // update value if have scroll value
+        if (scrollDelta != 0)
+        {
+            // update scrollbar value between 0 and 1
+            SkillAbilityscrollbar.value += scrollDelta * scrollSpeed;
+            SkillAbilityscrollbar.value = Mathf.Clamp01(SkillAbilityscrollbar.value);
+        }
+
+        if (isIncreasing)
+        {
+            isDecreasing = false;
+            // value +0.2 per sec
+            SkillAbilityscrollbar.value += 3f * Time.deltaTime;
+            if (SkillAbilityscrollbar.value >= 1f)
+            {
+                SkillAbilityscrollbar.value = 1f; // make sure that is not more than 1
+                isIncreasing = false; // stop growing
+                UpdateButtonActive();
+            }
+        }
+
+        if (isDecreasing) 
+        {
+            isIncreasing = false;
+            // value -0.2 per sec
+            SkillAbilityscrollbar.value -= 3f * Time.deltaTime;
+            if (SkillAbilityscrollbar.value <= 0f)
+            {
+                SkillAbilityscrollbar.value = 0f; // make sure that is 0
+                isDecreasing = false; // stop going down
+                UpdateButtonActive();
+            }
+        }
+
+        
+        if (SkillOrAbilityPanel.activeInHierarchy == false) 
+        { 
+            skillOrAbilityButton.enabled = false;
+            SkillClickArea.SetActive(true);
+            AbilityClickArea.SetActive(true);
+        }
+        else
+        {
+            skillOrAbilityButton.enabled = true;
+            SkillClickArea.SetActive(false);
+            AbilityClickArea.SetActive(false);
+        }
+        // control button active
+        UpdateButtonActive();
+
+        // update CyanSelectedBlock position and scale according to the scrollbar value
+        UpdateCyanSelectedBlock();
+    }
+
+    public void ShowSkillAbilityPanel()
+    { 
+        SkillOrAbilityPanel.SetActive(true);
+        SpecialAttackPanel.SetActive(false);
+    }
+
+    public void ShowSkillPart() { SkillAbilityscrollbar.value = 0f; }
+
+    public void ShowAbilityPart() { SkillAbilityscrollbar.value = 1f; }
+    private void OnArrowButtonClick()
+    {
+        isIncreasing = true; 
+    }
+
+    private void OnSpecialAttackButtonClick()
+    {
+        SkillOrAbilityPanel.SetActive(false);
+        SpecialAttackPanel.SetActive(true);
+
+        //reset special attack scrollbar
+        SpecialAttackscrollbar.value = 1f;
+
+        // set scale to 0.5, and back to 1 in 0.2 sec
+        specialAttackButton.transform.localScale = Vector3.one * 0.5f;
+        specialAttackButton.transform.DOScale(Vector3.one, 0.2f);
+    }
+
+    private void UpdateButtonActive()
+    {
+        // value < 0.7, ArrowButton setActive true
+        arrowButton.gameObject.SetActive(SkillAbilityscrollbar.value < 0.7f);
+    }
+
+    private void OnEseButtonClick() 
+    {
+        specialAttackButton.transform.localScale = Vector3.one * 0.5f;
+        specialAttackButton.transform.DOScale(Vector3.one, 0.2f);
+
+        GamingUI.SetActive(false);
+    }
+
+    private void OnSkillOrAbilityButtonClick()
+    {
+        ShowSkillAbilityPanel();
+
+        // set scale to 0.5, and back to 1 in 0.2 sec
+        skillOrAbilityButton.transform.localScale = Vector3.one * 0.5f;
+        skillOrAbilityButton.transform.DOScale(Vector3.one, 0.2f);
+
+        if (SkillAbilityscrollbar.value < 0.6) { isIncreasing = true; }
+        else { isDecreasing = true; }
+
+    }
+    
+    private void UpdateCyanSelectedBlock()
+    {
+        if (SpecialAttackPanel.activeInHierarchy == true) 
+        {
+            cyanSelectedBlock.anchoredPosition = new Vector2(275, 8);
+            cyanSelectedBlock.sizeDelta = new Vector2(365, cyanSelectedBlock.sizeDelta.y);
+        }
+
+        else if (SkillAbilityscrollbar.value > 0.6f)
+        {
+            cyanSelectedBlock.anchoredPosition = new Vector2(-138, 8);
+            cyanSelectedBlock.sizeDelta = new Vector2(185, cyanSelectedBlock.sizeDelta.y);
+        }
+        else
+        {
+            cyanSelectedBlock.anchoredPosition = new Vector2(-290, 8);
+            cyanSelectedBlock.sizeDelta = new Vector2(145, cyanSelectedBlock.sizeDelta.y);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UISpawner.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+using UnityEngine.UI;
+
+public class UISpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numberOfPrefabs = 15;
+    public Transform spawnParent;
+
+    public Scrollbar SpecialAttackscrollbar;
+    public float scrollSpeed = 0.1f;
+
+    // KeywordTexts' content
+    private string[] keywordTexts = {
+        "<color=yellow>Fire Tornado</color>: Generate a massive fire tornado in the arena to damage the opponent.",
+        "<color=yellow>Strong Winds</color>: Whip up a strong wind in the arena to hinder the opponent's movement.",
+        "<color=yellow>Guess Who I Am</color>: Allow a helper to obstruct the opponent's line of sight.",
+        "<color=yellow>Double Damage</color>: Cause the opponent to take double damage from enemies and traps.",
+        "<color=yellow>Preemptive Strike</color>: Prevent the opponent from using Special Attack for 30 seconds.",
+        "<color=yellow>Bondage</color>: Disable the opponent's ability to roll and block.",
+        "<color=yellow>Set the Bomb</color>: Place bombs on obstacles in the opponent's arena; touching the obstacles will trigger explosive damage.",
+        "<color=yellow>Give Up Treatment</color>: Prevent the opponent from recovering HP by any means for 2 minutes.",
+        "<color=yellow>Poison</color>: Directly inflict three random debuffs on the opponent for 30 seconds.",
+        "<color=yellow>Being a Poor Person</color>: Prevent the opponent from acquiring any coins or items for 1 minute.",
+        "<color=yellow>Tax Hikes</color>: Force the opponent to pay double the price for certain items at their next encounter with a shop.",
+        "<color=yellow>Cheer up</color>: Heal the enemies in the opponent's arena (restore 50% HP and increase movement speed by 5%).",
+        "<color=yellow>Evil Will Be Punished</color>: If the opponent holds an item that negatively affects themselves, the item's effect will apply directly to the opponent.",
+        "<color=yellow>Enemies From The Sky</color>: Random enemies will drop from the sky above the opponent, potentially causing trouble.",
+        "<color=yellow>Nightmare</color>: Generate an enemy with invincibility effects to chase the opponent; this enemy will only disappear after the opponent passes through a level portal."
+    };
+
+    void Start()
+    {
+        SpawnPrefabs();
+    }
+
+    void Update()
+    {
+        // get mouse scroll value
+        float scrollDelta = Input.mouseScrollDelta.y;
+
+        // update value if have scroll value
+        if (scrollDelta != 0)
+        {
+            // update scrollbar value between 0 and 1
+            SpecialAttackscrollbar.value += scrollDelta * scrollSpeed;
+            SpecialAttackscrollbar.value = Mathf.Clamp01(SpecialAttackscrollbar.value);
+        }
+    }
+
+    void SpawnPrefabs()
+    {
+        for (int i = 0; i < numberOfPrefabs; i++)
+        {
+            //spawn prefab
+            GameObject instance = Instantiate(prefab, spawnParent);
+
+            // update spwan name
+            instance.name = "KW (" + (i + 1) + ")";
+
+            // change TMP text
+            TextMeshProUGUI numText = instance.transform.Find("Num").GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI keywordText = instance.transform.Find("KeywordTexts").GetComponent<TextMeshProUGUI>();
+
+            if (numText != null)
+            {
+                numText.text = "#" + (i + 1); // #1, #2, ...
+            }
+
+            if (keywordText != null && i < keywordTexts.Length)
+            {
+                keywordText.text = keywordTexts[i]; // Generate keywords in order
+            }
+        }
+
+        // refresh Layout
+        LayoutRebuilder.ForceRebuildLayoutImmediate(spawnParent.GetComponent<RectTransform>());
+
+    }
+    
+}
