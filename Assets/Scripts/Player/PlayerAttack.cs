37192176430abfafa8823ae1cc5aec28a9b0958d
QP12579 <164116570+QP12579@users.noreply.github.com>
2025-04-26 02:55:57 +0800

    Remember add type to player skill controller's skill data, if no type, their function will not be run

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Player))]
 public class PlayerAttack : MonoBehaviour
 {
     [Header(" Components ")]
