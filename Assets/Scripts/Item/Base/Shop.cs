1f3d9176817a42fd2d76b5fea6a31bdd02621889
QP12579 <164116570+QP12579@users.noreply.github.com>
2025-05-11 05:39:08 +0800

    quak

---
+++ Assets/Scripts/Item/Base/Shop.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> spawnPoints;
+    [SerializeField]
+    private List<GameObject> spawnItems;
+    [SerializeField]
+    private List<ItemType> canSpawntype;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnItems()
+    {
+
+    }
+}
