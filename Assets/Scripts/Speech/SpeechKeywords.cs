96c58e371163cf1477f57242c6787d467222a3bb
cky314 <164114393+cky314@users.noreply.github.com>
2025-05-10 22:29:34 +0800

    Add Speech Recognition

---
+++ Assets/Scripts/Speech/SpeechKeywords.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+public class SpeechKeywords : MonoBehaviour
+{
+    public TMP_Text outputText;
+    public List<string> keywords; 
+    public List<GameObject> vfxPrefabs; 
+    private HashSet<string> generatedKeywords = new HashSet<string>();
+
+    private void Update()
+    {
+        CheckForKeyword();
+        Debug.Log("Checked for keyword");
+    }
+
+    private void CheckForKeyword()
+    {
+        string speechword = RemovePunctuation(outputText.text.Trim());
+        Debug.Log($"Current output: {speechword}");
+
+        for (int i = 0; i < keywords.Count; i++)
+        {
+            if (speechword.Equals(keywords[i], System.StringComparison.OrdinalIgnoreCase) && !generatedKeywords.Contains(keywords[i]))
+            {
+                Debug.Log($"Keyword matched: {keywords[i]}! Generating VFX.");
+                GenerateVFX(i);
+                generatedKeywords.Add(keywords[i]);
+                break;
+            }
+        }
+    }
+
+    private string RemovePunctuation(string input)
+    {
+        return Regex.Replace(input, @"[^\w\s]", "");
+    }
+
+    private void GenerateVFX(int index)
+    {
+        GameObject clonedVFX = Instantiate(vfxPrefabs[index], Vector3.zero, Quaternion.identity);
+        Destroy(clonedVFX, 5f);
+        Debug.Log("VFX cloned");
+    }
+}
