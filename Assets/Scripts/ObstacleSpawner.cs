a61d7ea0eaf6b8cf407d59207a94188867ce9a8d
cky314 <164114393+cky314@users.noreply.github.com>
2025-05-12 05:13:36 +0800

    Random spawn traps and obstacles + Level Events

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs; 
+    public Transform[] spawnPoints;
+    public GameObject[] trapPrefabs;
+    public Transform[] trapSpawnPoints;
+    public int numberOfObstacles = 5;
+    public int numberOfTraps = 3;
+
+    public GameObject specificTrapPrefab;
+    public int maxSpecificTrapCount = 3;
+
+    void Start()
+    {
+        SpawnObstacles();
+        SpawnTraps();
+    }
+
+    void SpawnObstacles()
+    {
+        List<Transform> availableSpawnPoints = new List<Transform>(spawnPoints);
+
+        for (int i = 0; i < numberOfObstacles; i++)
+        {
+            if (availableSpawnPoints.Count == 0) break;
+
+            int randomIndex = Random.Range(0, availableSpawnPoints.Count);
+            Transform spawnPoint = availableSpawnPoints[randomIndex];
+
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+
+            Instantiate(obstaclePrefab, spawnPoint.position, spawnPoint.rotation);
+
+            availableSpawnPoints.RemoveAt(randomIndex);
+        }
+    }
+
+    void SpawnTraps()
+    {
+        List<Transform> availableTrapSpawnPoints = new List<Transform>(trapSpawnPoints);
+        int specificTrapCount = 0;
+
+        for (int i = 0; i < numberOfTraps; i++)
+        {
+            if (availableTrapSpawnPoints.Count == 0) break;
+
+            GameObject trapPrefab;
+            if (specificTrapCount < maxSpecificTrapCount && Random.value < 0.5f) 
+            {
+                trapPrefab = specificTrapPrefab;
+                specificTrapCount++;
+            }
+            else
+            {
+                trapPrefab = trapPrefabs[Random.Range(0, trapPrefabs.Length)];
+            }
+
+            int randomIndex = Random.Range(0, availableTrapSpawnPoints.Count);
+            Transform trapSpawnPoint = availableTrapSpawnPoints[randomIndex];
+
+            Instantiate(trapPrefab, trapSpawnPoint.position, trapSpawnPoint.rotation);
+            availableTrapSpawnPoints.RemoveAt(randomIndex);
+        }
+    }
+}
